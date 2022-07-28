49da3366cabaf0155b834aa60f173cf4e2215515
Riches <richeskelechi94.rk@gmail.com>
2022-07-28 16:21:53 +0100

    Added the login and register routes

--- backend/Homeviews/urls.py
+++ backend/Homeviews/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home_view, name="home"),
     path('about', views.about_view, name="about"),
     path('contact', views.contact_view, name="contact"),
+    path('login', views.login_view, name="login"),
+    path('register', views.register_view, name="register"),
 ]
