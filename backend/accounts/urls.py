c72b7f5e520c24f64b030d946ab1fc51e6e06946
Riches <richeskelechi94.rk@gmail.com>
2022-08-03 19:05:03 +0100

    Done

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup/', views.signup_view, name="signup"),
     path('signin/', views.signin_view, name="signin"),
     path('logout/', views.logout_view, name="logout"),
+    path('profile/', views.profile_view, name="profile"),
 ]
