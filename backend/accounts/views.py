b84a75c9e36323695f0b05b247714b1fdc4dcacf
Riches <richeskelechi94.rk@gmail.com>
2022-08-03 18:13:01 +0100

    Fixed the registration, login and logout page.

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserAuthenticationForm
 
 
 def signup_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # login(request, user)
+            user = form.save()
+            login(request, user)
             return redirect('dashboard:home')
         else:
             # print(form.errors.as_data())
@@ -20,10 +20,24 @@ def signup_view(request):
 
 
 def signin_view(request):
-    # return render(request, 'homepage.html')
-    return HttpResponse("Login page")
+    if request.method == 'POST':
+        form = CustomUserAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            return redirect('dashboard:home')
+        else:
+            return render(request, 'accounts/login.html', {'form': form})
+    else:
+        form = CustomUserAuthenticationForm()
+    return render(request, "accounts/login.html", {'form': form})
 
 
 def logout_view(request):
-    # return render(request, 'contactpage.html')
-    return HttpResponse("Logout page")
+    if request.method == 'POST':
+        logout(request)
+        return redirect('accounts/login.html')
+    else:
+        return redirect('accounts/login.html')
