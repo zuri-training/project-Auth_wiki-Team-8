ae15ce5f460e3e13899f962dcd6a43c281a62596
Riches <richeskelechi94.rk@gmail.com>
2022-08-03 07:39:54 +0100

    done

--- backend/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup/', views.UserCreateView.as_view(), name='sign_up'),
-]
--- backend/library_app/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include, re_path
-
-urlpatterns = [
-    # path('library/', ),
-]
