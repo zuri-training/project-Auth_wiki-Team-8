a9fb60f196487e33a02502463b91b33978128766
Micheal David <micheal.mycareer@gmail.com>
2022-08-04 12:21:41 -0500

    make things better

--- backend/auth_wiki/urls.py
+++ backend/auth_wiki/urls.py
@@ -14,3 +14,10 @@ urlpatterns = [
     path('', views.home),
     # path('', article_views.article_list, name="home"),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
