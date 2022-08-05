3b13b2dd5b9a8736d44dfecd5fc25f899b1850d9
Micheal David <micheal.mycareer@gmail.com>
2022-08-05 17:10:57 -0500

    make it better

--- backend/librarys/views.py
+++ backend/librarys/views.py
@@ -9,7 +9,8 @@ class LibrarySearchPage(View):
         if 'q' in request.GET:
             keyword = request.GET['q']
             libraries = LibraryPage.objects.filter(name__contains=keyword)
-            return render(request, 'librarys/library_page.html', {'libraries': libraries})
+            found = len(libraries)
+            return render(request, 'librarys/search_result.html', {'libraries': libraries, 'found': found, 'title': 'Search results'})
         else:
             return render(request, 'librarys/index.html')
 
@@ -18,5 +19,5 @@ class LibraryInfo(View):
     def get(self, request, *args, **kwargs):
         pk = request.GET.get('pk')
         library = LibraryPage.objects.filter(id=pk)
-        return render(request, 'librarys/search_result.html', {'library': library})
+        return render(request, 'librarys/library_page.html', {'library': library})
 
