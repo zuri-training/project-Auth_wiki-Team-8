8740aaa92eff593e575a5ee5d62be33ebc405d4e
godwin <okpegodwin18@yahoo.com>
2022-08-05 22:52:50 +0100

    Made few changes to the settings and urls for the accounts app

--- backend/auth_wiki/settings.py
+++ backend/auth_wiki/settings.py
@@ -115,3 +115,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
