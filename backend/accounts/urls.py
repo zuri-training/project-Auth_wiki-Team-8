47a21e2e76581a5de995887cd80f0f97af597ccf
godwin <okpegodwin18@yahoo.com>
2022-08-06 12:46:45 +0100

    Created custom email for the password reset

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
          name="password_change"),
     path('password_change/done/', PasswordChangeDoneView.as_view(),
          name="password_change_done"),
-    path('password_reset/', PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset/', PasswordResetView.as_view(email_template_name='accounts/reset_pass_email.html'), name="password_reset"),
     path('password_reset/done/', PasswordResetDoneView.as_view(),
          name="password_reset_done"),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
