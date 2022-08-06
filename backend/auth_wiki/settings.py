d0cb64ba212a36a49b09cdc7b213ac30525ec490
godwin <okpegodwin18@yahoo.com>
2022-08-06 11:21:32 +0100

    Minor fixes in login as well as urls for password reset. For now, password reset email is set to be displayed on the console. This can be easily changed when we are ready for production

--- backend/auth_wiki/settings.py
+++ backend/auth_wiki/settings.py
@@ -127,3 +127,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
