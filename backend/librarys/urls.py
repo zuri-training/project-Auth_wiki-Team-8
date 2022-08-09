ee8e6f9bb78bed5194b6f75bdd16c1c541ea5102
Micheal David <micheal.mycareer@gmail.com>
2022-08-09 07:38:00 -0500

    make it better

--- backend/librarys/urls.py
+++ backend/librarys/urls.py
@@ -6,5 +6,4 @@ app_name = 'librarys'
 
 urlpatterns = [
     path('', library_views.LibrarySearchPage.as_view(), name="home"),
-    path('search_result/<pk>', library_views.LibraryInfo.as_view(), name='search_result')
 ]
