134dbf06ede591d0125fc1b9dc1e66b948371d4b
Micheal David <micheal.mycareer@gmail.com>
2022-08-14 20:21:26 -0500

    multiple likes and dislikes solved

--- backend/librarys/admin.py
+++ backend/librarys/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import CommentReaction, LibraryPage
+from .models import CommentReaction, LibraryPage, Reactions
 
 # Register your models here.
 
 
 admin.site.register(LibraryPage)
 admin.site.register(CommentReaction)
-
+admin.site.register(Reactions)
